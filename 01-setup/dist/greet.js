1855237be69812c46578dddd7eb45066b7d56f1f
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-06-21 19:00:13 +0530

    initial setup

---
+++ 01-setup/dist/greet.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}`;
+}
+console.log(greet("Arbaj"));
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+// console.log(add(5, "10")); // ❌ Compiler error: Argument of type 'string' is not assignable to parameter of type 'number' 
+//# sourceMappingURL=greet.js.map
---
+++ 01-setup/src/greet.ts
@@ -0,0 +1,11 @@
+function greet(name: String): String {
+  return `Hello, ${name}`;
+}
+
+console.log(greet("Arbaj"));
+
+
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+// console.log(add(5, "10")); // ❌ Compiler error: Argument of type 'string' is not assignable to parameter of type 'number' 

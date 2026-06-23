a55612a23b285da15312219d16cb4c878100f077
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-06-23 11:31:22 +0530

    learned about Functions: optional params, overloads, arrow functions

--- 07-Functions/src/index.ts
+++ 07-Functions/src/index.ts
@@ -13,3 +13,45 @@ console.log("greet 2 calling");
 
 greet2("arbaj");
 greet2("arbaj", 23);
+
+
+
+// arrow functions
+const add = (a:number, b:number):number => a + b;
+
+console.log("\ncalling add")
+console.log(add(5, 10));
+console.log(add(2, 3));
+console.log(add(3, 4));
+
+
+// function overloading 
+
+function display (value : string):void;
+function display (value : number):void;
+
+function display (value : string | number):void {
+
+    if(typeof value == "string"){
+        console.log(`this is the string : ${value}`);        
+    }else {
+        console.log(`this is the number : ${value}`);
+    }
+}
+
+console.log("\ncalling display");   
+display("arbaj");
+display(22);
+
+
+// rest parameters 
+function rest(...numbers:number[]):void{
+
+    const addition = numbers.reduce((res, ele)=> res+= ele);
+    console.log(`addition is : ${addition}`);
+
+}
+
+console.log("\ncalling rest for addition ");   
+rest(1,2,3);
+rest(1,2,3,4,5);
